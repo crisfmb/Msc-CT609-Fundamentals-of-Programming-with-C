3ec521895872b370e1701823de97b79faebec31f
cristina <crisfmb@gmail.com>
2021-01-14 23:00:17 +0000

    commit first version examWeek8

--- MscWeek8Exam/MscExam.c
+++ MscWeek8Exam/MscExam.c
@@ -80,5 +80,121 @@ Function 11: save_data [20 marks]
 
 This function is called when the user chooses to exit the system. Open a data file and write
 out the information contained in the array.
-*/
 
+ - salario < 151.50 < 203 por semana o subsidio será = 203 semanal
+ - salario > 203    < 300 por semana o subsidio será = 250
+ - salario > 300    < 400 por semana o subsidio será = 300
+ - salario > 400    < 1462
+
+  MIN >             MAX <=      SUBSIDY
+  0                 151.50    0  
+  151.50            203       203
+  203               300       250 
+  300               400       300
+  400               1462      350 
+  
+  insurable weeks precisa ser entre 1 e 5 ou subsidy será 0
+employee_code (int) 
+employee_name (char[]) 
+insurable_weeks (int)
+gross_pay (float)
+subsidy (float)
+
+1- Ler o arquivo data.txt
+    SE FUNCIONAR(arquivo existir) -> 
+        -carrega funcionários (dinamicamente no formato respeitando o struct)
+    SENÃO FUNCIONAR(arquivo não existir) -> 
+        -notifica o usuário
+        -cria um arquivo novo chamado Data.txt
+        
+2- -exibe menu
+
+new bolas[]
+
+bola [tamanho: , cor:, marca:]
+
+bola.tamanho = pequena
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Employee /*struct e um tipo de modelo de dados ou variavel que contem variaveis dentro que podem ser de diferentes tipos. Todas as variaveis dentro de um vetor precisam ser do mesmo tipo e no struct não. NÃO ESQUECE A DIFERENÇA.
+Nesse caso criei um modelo para armazenar as informações dos funcionários como o professor pediu no primeiro enunciado */
+{
+    int employee_code;
+    char employee_name[1000];
+    int insurable_weeks;
+    float gross_pay;
+    float subsidy;
+};
+
+void add_new_employee() //a primeira opção do menu pede pra chamar uma nova funcao onde add um novo funcionario, esse e o enunciado 2 do documento.(Função adiciona novo funcionário ao final do arquivo)
+{
+
+    struct Employee newEmployee;
+    printf("Digite o código: ");
+    scanf("%d", &newEmployee.employee_code);
+
+    printf("%d", newEmployee.employee_code);
+}
+
+void display_menu() //aqui cria a função display_menu do enunciado dois. Exibe as opções do menu. só copiei e colei o menu do documento.
+{
+
+    int opcao;
+    printf("1. Add new employee: \n");
+    printf("2. Delete employee: \n");
+    printf("3. View all employees in the list: \n");
+    printf("4. View only qualifying employees: \n");
+    printf("5. View disqualified employees (and show reason): \n");
+    printf("6. Sort employees by name: \n");
+    printf("7. Sort employees by subsidy amount: \n");
+    printf("8. View total subsidy amount (total for all employees): \n");
+    printf("9. Exit: \n");
+    printf("Digite uma opção: ");
+    scanf("%d", &opcao);
+    switch (opcao) //decidi colocar o switch aqui e não na função main pq no enunciado diz pra você ler uma opção. foi uma escolha ja que nao foi especificado nada.
+    {
+    case 1: // se a opção selecionada for a 1 faz o calculo de soma ou qualquer outra operação e chama a função de adicionar novo funcionario
+        add_new_employee();
+        break;
+    case 2: // se a opção selecionada for a 2 faz o calculo de soma ou qualquer outra operação e imprime o valor
+        printf("Opcao 2 do Menu");
+        break;
+    case 3:
+        printf("Opcao 3 do Menu");
+        break;
+    case 4:
+        printf("Opcao 4 do Menu");
+        break;
+    case 5:
+        printf("Opcao 5 do Menu");
+        break;
+
+    case 6:
+        printf("Opcao 6 do Menu");
+        break;
+    case 7:
+        printf("Opcao 7 do Menu");
+        break;
+    case 8:
+        printf("Opcao 8 do Menu");
+        break;
+    case 9:
+        printf("Opcao 9 do Menu");
+        break;
+
+    default: // se não for nenhuma das opções acima imprime opcao invalida
+        printf("Opção invalida");
+        break;
+    }
+}
+
+int main() // inicia função principal
+{
+    display_menu();
+
+    return 0;
+} // SOCORRO SOCORRO SOCORRO
